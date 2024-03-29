14af2c4d0ea04f984616602bad5238afc965469f
ChristianNeitzel <pg52526@uminho.pt>
2024-03-29 18:19:53 +0000

    adicionado ficheiros, atualizado trabalho.R

--- trabalho.R
+++ trabalho.R
@@ -2,11 +2,10 @@
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
-# Verifica se o cBioPortalData está instalado e instala se necessário
 if (!requireNamespace("cBioPortalData", quietly = TRUE))
   BiocManager::install("cBioPortalData")
 
-# Carrega o pacote cBioPortalData
+# Carregamento dos packages
 library(cBioPortalData)
 
 # Inicialização do API cBioPortal
@@ -18,15 +17,17 @@ bladder_clin
 dim(bladder_clin)  # Deve retornar 526 linhas e 29 colunas
 
 # Carrega os dados do cBioPortal
-bladder = cBioDataPack("bladder_msk_2023")
+bladder = cBioDataPack("bladder_msk_2023", ask = FALSE)
+bladder
 
+################################################################################
 # Lista os nomes dos ensaios disponíveis no pacote dos dados do CBioPortal
-ensaios_disponiveis = names(assays(bladder))
-ensaios_disponiveis
+names(assays(bladder))
+# names(rowData(bladder)) # tipos de metadados associados a cada gene
+# names(colData(bladder)) # tipos de metadados associados a cada amostra
 
-# Resumo dos dados do pacote bladder
-resumo_bladder = summary(bladder)
-resumo_bladder
+# Summary dos dados
+summary(bladder)
 
 # Acessando os dados de cna e convertendo para um dataframe
 dados_cna = assays(bladder)$cna
@@ -43,91 +44,109 @@ dados_mutacao_df = as.data.frame(dados_mutacao)
 # Verificando a classe e dimensões dos dados de mutação
 dim(dados_mutacao_df)
 
-#######################################
+################################################################################
 
-# definir as variaveis com as.factor 
+## Definir as variaveis com as.factor 
+# age = as.factor(bladder_clin$AGE_AT_SEQ_REPORTED_YEARS)
+# cancer_type = as.factor(bladder_clin$CANCER_TYPE_DETAILED)
+# disease_state = as.factor(bladder_clin$DISEASE_STATE)
+# erdafitinib_treatment = as.factor(bladder_clin$ERDAFITINIB_TREATED)
+# overall_status = as.factor(bladder_clin$ERDAFITINIB_TX_OS_STATUS)
+# progression_status = as.factor(bladder_clin$ERDAFITINIB_TX_PFS_STATUS)
+# gene_panel = as.factor(bladder_clin$GENE_PANEL)
+# msi_type = as.factor(bladder_clin$MSI_TYPE)
+# oncotree_code = as.factor(bladder_clin$ONCOTREE_CODE)
+# race = as.factor(bladder_clin$RACE)
+# sample_class = as.factor(bladder_clin$SAMPLE_CLASS)
+# overall_seq = as.factor(bladder_clin$ERDAFITINIB_TX_OS_MONTHS)
+# sexo = as.factor(bladder_clin$SEX)
+# smoking_status = as.factor(bladder_clin$SMOKIMG_STATUS)
+# somatic_status = as.factor(bladder_clin$SOMATIC_STATUS)
 
-
-age = as.factor(bladder_clin$AGE_AT_SEQ_REPORTED_YEARS)
-cancer_type_detailed = as.factor(bladder_clin$CANCER_TYPE_DETAILED)
-disease_state = as.factor(bladder_clin$DISEASE_STATE)
-erdafitinib_treatment = as.factor(bladder_clin$ERDAFITINIB_TREATED)
-overall_status = as.factor(bladder_clin$OVERALL_SURVIVAL_STATUS_SINCE_ERDAFITINIB_TX)
-#progression_status = as.factor(bladder_clin$PROGRESSION_FREE_STATUS_SINCE_ERDAFITINIB_TX)
-gene_panel = as.factor(bladder_clin$GENE_PANEL)
-msi_type = as.factor(bladder_clin$MSI_TYPE)
-oncotree_code = as.factor(bladder_clin$ONCOTREE_CODE)
-race = as.factor(bladder_clin$RACE)
-sample_class = as.factor(bladder_clin$SAMPLE_CLASS)
-overall_seq = as.factor(bladder_clin$OVERALL_SURVIVAL_STATUS_SINCE_SEQUENCING)
-sexo = as.factor(bladder_clin$SEX)
-smoking_status = as.factor(bladder_clin$SMOKING_STATUS)
-somatic_status = as.factor(bladder_clin$SOMATIC_STATUS)
-
-
-
-"""
+"
 1. Variáveis Numéricas:
-   - Age at Which Sequencing was Reported (Years):
-     - **Análise Estatística Descritiva:** Calcule medidas de tendência central (média, mediana) e dispersão (desvio padrão, intervalo) para entender a distribuição da idade dos pacientes.
-     - **Exploração com Gráficos:** Um histograma pode ser útil para visualizar a distribuição da idade dos pacientes.
- """
-
-# Remover valores ausentes e calcular o valor minimo, 1º quartil, mediana, média, 3º quartil e valor máximo da variavel.
-summary_age <- summary(na.omit(bladder_msk_2023_clinical_data$"Age.at.Which.Sequencing.was.Reported..Years."))
-# Visualizar os valores
-summary_age
-
-# Remover valores ausentes e calcular o desvio padrão da variavel.
-sd_age <- sd(na.omit(bladder_msk_2023_clinical_data$"Age.at.Which.Sequencing.was.Reported..Years."))
-# Visualizar o valor
-sd_age
-
-# Remover valores ausentes e calcular o Intervalo interquartilico.
-IQR_age <- IQR(na.omit(bladder_msk_2023_clinical_data$"Age.at.Which.Sequencing.was.Reported..Years."))
-# Visualizar o valor
-IQR_age
-
-barplot(table(bladder_msk_2023_clinical_data$"Age.at.Which.Sequencing.was.Reported..Years."))
-plot(table(bladder_msk_2023_clinical_data$"Age.at.Which.Sequencing.was.Reported..Years."))
-
-# Criar o gráfico de caixa
-boxplot(bladder_msk_2023_clinical_data$"Age.at.Which.Sequencing.was.Reported..Years.",
-        main = "Idade em que a Sequência foi Reportada (Anos)", xlab = "Idade", horizontal=T)
-
-
-"""
+   - Age at Which Sequencing was Reported (Years)
    - Overall Survival Months since Erdafitinib Tx
    - Progress Free Survival Months since Erdafitinib Tx
    - Overall Survival Months since Sequencing
-     - **Análise Estatística Descritiva:** Calcule medidas de tendência central e dispersão para entender a distribuição dos períodos de sobrevivência e de progressão.
-     - **Exploração com Gráficos:** Um histograma ou um boxplot pode ser útil para visualizar a distribuição dos períodos de sobrevivência e de progressão.
-"""
-# Remover valores ausentes e calcular o valor minimo, 1º quartil, mediana, média, 3º quartil e valor máximo da variavel.
-summary_overall_survival_months_since_Er <- summary(na.omit(bladder_msk_2023_clinical_data$Overall.Survival..Months.since.Erdafitinib.Tx.))
-# Visualizar os valores
-summary_overall_survival_months_since_Er
+     - **Análise Estatística Descritiva:** Calcule medidas de tendência central 
+     (média, mediana) e dispersão (desvio padrão, intervalo) para entender a 
+     distribuição da idade dos pacientes.
+     - **Exploração com Gráficos:** Um histograma pode ser útil para visualizar 
+     a distribuição da idade dos pacientes.
+"
 
-# Remover valores ausentes e calcular o desvio padrão da variavel.
-sd_overall_survival_months_since_Er <- sd(na.omit(bladder_msk_2023_clinical_data$Overall.Survival..Months.since.Erdafitinib.Tx.))
-# Visualizar o valor
-sd_overall_survival_months_since_Er
+## Age at Which Sequencing was Reported (Years) (AGE_AT_SEQ_REPORTED_YEARS)
+# Remover valores ausentes e calcular o valor mínimo, 1º quartil, mediana, média, 3º quartil e valor máximo da variável.
+summary(na.omit(bladder_clin$AGE_AT_SEQ_REPORTED_YEARS))
 
-# Remover valores ausentes e calcular o Intervalo interquartilico.
-IQR_overall_survival_months_since_Er <- IQR(na.omit(bladder_msk_2023_clinical_data$Overall.Survival..Months.since.Erdafitinib.Tx.))
-# Visualizar o valor
-IQR_overall_survival_months_since_Er
+# Remover valores ausentes e calcular o desvio padrão da variável.
+sd(na.omit(bladder_clin$AGE_AT_SEQ_REPORTED_YEARS))
 
-barplot(table(bladder_msk_2023_clinical_data$Overall.Survival..Months.since.Erdafitinib.Tx.))
-plot(table(bladder_msk_2023_clinical_data$Overall.Survival..Months.since.Erdafitinib.Tx.))
+# Remover valores ausentes e calcular o Intervalo interquartílico.
+IQR(na.omit(bladder_clin$AGE_AT_SEQ_REPORTED_YEARS))
 
-# Criar o gráfico de caixa
-boxplot(bladder_msk_2023_clinical_data$Overall.Survival..Months.since.Erdafitinib.Tx.,
-        main = "Sobrevivência Geral (Meses desde o Tratamento com Erdafitinib)", xlab = "Meses", horizontal=T)
+# Barplot e boxplot da distribuição de idades
+barplot(table(bladder_clin$AGE_AT_SEQ_REPORTED_YEARS))
+boxplot(as.numeric(bladder_clin$AGE_AT_SEQ_REPORTED_YEARS, 
+        main = "Idade em que a Sequência foi Reportada (Anos)", 
+        xlab = "Idade", horizontal = TRUE))
 
 
 
-"""
+## Overall Survival Months since Erdafitinib Tx (ERDAFITINIB_TX_OS_MONTHS)
+# Remover valores ausentes e calcular o valor mínimo, 1º quartil, mediana, média, 3º quartil e valor máximo da variável.
+summary(na.omit(bladder_clin$ERDAFITINIB_TX_OS_MONTHS))
+
+# Remover valores ausentes e calcular o desvio padrão da variável.
+sd(na.omit(bladder_clin$ERDAFITINIB_TX_OS_MONTHS))
+
+# Remover valores ausentes e calcular o Intervalo interquartílico.
+IQR(na.omit(bladder_clin$ERDAFITINIB_TX_OS_MONTHS))
+
+# Barplot e boxplot
+barplot(table(bladder_clin$ERDAFITINIB_TX_OS_MONTHS))
+boxplot(as.numeric(bladder_clin$ERDAFITINIB_TX_OS_MONTHS,
+        main = "Sobrevivência Geral (Meses desde o Tratamento com Erdafitinib)", 
+        xlab = "Meses", 
+        horizontal = TRUE))
+
+
+
+## Progress Free Survival Months since Erdafitinib Tx (ERDAFITINIB_TX_PFS_MONTHS)
+# Remover valores ausentes e calcular o valor mínimo, 1º quartil, mediana, média, 3º quartil e valor máximo da variável.
+summary(na.omit(bladder_clin$ERDAFITINIB_TX_PFS_MONTHS))
+
+# Remover valores ausentes e calcular o desvio padrão da variável
+sd(na.omit(bladder_clin$ERDAFITINIB_TX_PFS_MONTHS))
+
+# Remover valores ausentes e calcular o Intervalo interquartílico
+IQR(na.omit(bladder_clin$ERDAFITINIB_TX_PFS_MONTHS))
+
+# Barplot e boxplot
+barplot(table(bladder_clin$ERDAFITINIB_TX_PFS_MONTHS))
+boxplot(as.numeric(bladder_clin$ERDAFITINIB_TX_PFS_MONTHS, 
+        horizontal = TRUE))
+
+
+
+## Overall Survival Months since Sequencing (SEQUENCING_OS_MONTHS)
+# Remover valores ausentes e calcular o valor mínimo, 1º quartil, mediana, média, 3º quartil e valor máximo da variável.
+summary(na.omit(bladder_clin$SEQUENCING_OS_MONTHS))
+
+# Remover valores ausentes e calcular o desvio padrão da variável
+sd(na.omit(bladder_clin$SEQUENCING_OS_MONTHS))
+
+# Remover valores ausentes e calcular o Intervalo interquartílico.
+IQR(na.omit(bladder_clin$SEQUENCING_OS_MONTHS))
+
+# Barplot e boxplot
+barplot(table(bladder_clin$SEQUENCING_OS_MONTHS))
+boxplot(as.numeric(bladder_clin$SEQUENCING_OS_MONTHS, 
+        horizontal = TRUE))
+
+
+"
 2. **Variáveis Categóricas:**
    - Cancer Type
    - Cancer Type Detailed
@@ -140,26 +159,28 @@ boxplot(bladder_msk_2023_clinical_data$Overall.Survival..Months.since.Erdafitini
    - Sex
    - Smoking Status
    - Somatic Status
-     - **Análise Estatística Descritiva:** Calcule as frequências absolutas e relativas para cada categoria.
+     - **Análise Estatística Descritiva:** Calcule as frequências absolutas e 
+     relativas para cada categoria.
      - **Exploração com Gráficos:** Gráficos de barras ou gráficos de pizza podem ser úteis para visualizar a distribuição das diferentes categorias.
    - MSI Type
    - Oncotree Code
-     - **Análise Estatística Descritiva:** Calcule as frequências absolutas e relativas para cada tipo de MSI (instabilidade de microssatélite) e para cada código Oncotree.
-     - **Exploração com Gráficos:** Gráficos de barras podem ser úteis para visualizar a distribuição dos tipos de MSI e dos códigos Oncotree.
-"""
+     - **Análise Estatística Descritiva:** Calcule as frequências absolutas e 
+     relativas para cada tipo de MSI (instabilidade de microssatélite) e para 
+     cada código Oncotree.
+     - **Exploração com Gráficos:** Gráficos de barras podem ser úteis para 
+     visualizar a distribuição dos tipos de MSI e dos códigos Oncotree.
+"
 
 ## Cancer Type - é semple "Blader Type"
-
 ## Cancer Type Detailed 
 
-
 # Gráfico de barras para visualizar as frequências absolutas
-barplot(table(bladder_msk_2023_clinical_data$Cancer.Type.Detailed), 
-        main = "Frequência Absoluta por Categoria",
-        xlab = "Categoria", ylab = "Frequência Absoluta")
+barplot(table(bladder_clin$CANCER_TYPE_DETAILED), 
+        main = "Frequência Absoluta por Tipo de Cancro",
+        xlab = "Tipo de Câncer", ylab = "Frequência Absoluta")
 
 # Calcular as frequências absolutas e relativas
-freq_abs <- table(bladder_msk_2023_clinical_data$Cancer.Type.Detailed)
+freq_abs <- table(bladder_clin$CANCER_TYPE_DETAILED)
 freq_rel <- prop.table(freq_abs)
 
 # Criar um data frame com as frequências absolutas e relativas
@@ -168,28 +189,30 @@ freq_data <- data.frame(Categoria = names(freq_abs),
                         Frequencia_Relativa = as.numeric(freq_rel))
 
 # Gráfico de barras para as frequências absolutas
-barplot(freq_abs, main = "Frequência Absoluta por Categoria",
-        xlab = "Categoria", ylab = "Frequência Absoluta")
+barplot(freq_abs, main = "Frequência Absoluta por Tipo de Câncer",
+        xlab = "Tipo de Câncer", ylab = "Frequência Absoluta")
 
 # Gráfico de barras para as frequências relativas
-barplot(freq_rel, main = "Distribuição Relativa por Categoria",
-        xlab = "Categoria", ylab = "Frequência Relativa")
+barplot(freq_rel, main = "Distribuição Relativa por Tipo de Câncer",
+        xlab = "Tipo de Câncer", ylab = "Frequência Relativa")
 
 # Criar o gráfico de pizza
-pie(freq_rel, main = "Distribuição Relativa por Categoria",
+pie(freq_rel, main = "Distribuição Relativa por Tipo de Cancro",
     labels = paste(names(freq_rel), ": ", round(freq_rel * 100, 2), "%"), 
     col = rainbow(length(freq_rel)),
     cex = 0.8)
 
+
+
 ## Disease State
 
 # Gráfico de barras para visualizar as frequências absolutas
-barplot(table(bladder_msk_2023_clinical_data$Disease.State), 
+barplot(table(bladder_clin$DISEASE_STATE), 
         main = "Frequência Absoluta por Categoria",
         xlab = "Categoria", ylab = "Frequência Absoluta")
 
 # Calcular as frequências absolutas e relativas
-freq_abs <- table(bladder_msk_2023_clinical_data$Disease.State)
+freq_abs <- table(bladder_clin$DISEASE_STATE)
 freq_rel <- prop.table(freq_abs)
 
 # Criar um data frame com as frequências absolutas e relativas
@@ -548,4 +571,117 @@ plot(table(bladder_msk_2023_clinical_data$Best.Response.to.Erdafitinib))
 # Criar o gráfico de caixa
 boxplot(bladder_msk_2023_clinical_data$Best.Response.to.Erdafitinib, 
         main = "Melhor resposta ao Erdafitinib", xlab = "", horizontal=T)
+
+
+"""
+3. Variáveis Contínuas:
+   - Fraction Genome Altered
+   - Mutation Count
+   - TMB (nonsynonymous)
+   - Tumor Purity
+  
+Análise Estatística Descritiva: Calcule medidas de tendência central e dispersão para entender a distribuição dessas variáveis contínuas.
+- Exploração com Gráficos: Um boxplot pode ser útil para visualizar a distribuição dessas variáveis.
+
+"""
+## Fraction Genome Altered
+
+# Remover valores ausentes e calcular o valor minimo, 1º quartil, mediana, média, 3º quartil e valor máximo da variavel.
+summary_ <- summary(na.omit(bladder_msk_2023_clinical_data$"Fraction.Genome.Altered"))
+# Visualizar os valores
+summary_
+
+# Remover valores ausentes e calcular o desvio padrão da variavel.
+sd_ <- sd(na.omit(bladder_msk_2023_clinical_data$"Fraction.Genome.Altered"))
+# Visualizar o valor
+sd_
+
+# Remover valores ausentes e calcular o Intervalo interquartilico.
+IQR_ <- IQR(na.omit(bladder_msk_2023_clinical_data$"Fraction.Genome.Altered"))
+# Visualizar o valor
+IQR_
+
+# Calcular a variância
+var_ <- var(na.omit(bladder_msk_2023_clinical_data$"Fraction.Genome.Altered"))
+
+# Visualizar o valor da variância
+var_
+
+# Criar o gráfico de caixa
+boxplot(bladder_msk_2023_clinical_data$"Fraction.Genome.Altered", horizontal=T)
+
+
+
+### Mutation Count
+# Remover valores ausentes e calcular o valor minimo, 1º quartil, mediana, média, 3º quartil e valor máximo da variavel.
+summary_ <- summary(na.omit(bladder_msk_2023_clinical_data$"Mutation.Count"))
+# Visualizar os valores
+summary_
+
+# Remover valores ausentes e calcular o desvio padrão da variavel.
+sd_ <- sd(na.omit(bladder_msk_2023_clinical_data$"Mutation.Count"))
+# Visualizar o valor
+sd_
+
+# Remover valores ausentes e calcular o Intervalo interquartilico.
+IQR_ <- IQR(na.omit(bladder_msk_2023_clinical_data$"Mutation.Count"))
+# Visualizar o valor
+IQR_
+
+# Calcular a variância
+var_ <- var(na.omit(bladder_msk_2023_clinical_data$"Mutation.Count"))
+
+# Visualizar o valor da variância
+var_
+
+barplot(table(bladder_msk_2023_clinical_data$"Mutation.Count"))
+
+# Criar o gráfico de caixa
+boxplot(bladder_msk_2023_clinical_data$"Mutation.Count", horizontal=T)
+
+
+### TMB (nonsynonymous)
+# Remover valores ausentes e calcular o valor minimo, 1º quartil, mediana, média, 3º quartil e valor máximo da variavel.
+summary_ <- summary(na.omit(bladder_msk_2023_clinical_data$"TMB..nonsynonymous."))
+# Visualizar os valores
+summary_
+
+# Remover valores ausentes e calcular o desvio padrão da variavel.
+sd_ <- sd(na.omit(bladder_msk_2023_clinical_data$"TMB..nonsynonymous."))
+# Visualizar o valor
+sd_
+
+# Remover valores ausentes e calcular o Intervalo interquartilico.
+IQR_ <- IQR(na.omit(bladder_msk_2023_clinical_data$"TMB..nonsynonymous."))
+# Visualizar o valor
+IQR_
+
+barplot(table(bladder_msk_2023_clinical_data$"TMB..nonsynonymous."))
+plot(table(bladder_msk_2023_clinical_data$"TMB..nonsynonymous."))
+
+# Criar o gráfico de caixa
+boxplot(bladder_msk_2023_clinical_data$"TMB..nonsynonymous.", horizontal=T)
+
+
+#### Tumor Purity
+# Remover valores ausentes e calcular o valor minimo, 1º quartil, mediana, média, 3º quartil e valor máximo da variavel.
+summary_ <- summary(na.omit(bladder_msk_2023_clinical_data$"Tumor.Purity"))
+# Visualizar os valores
+summary_
+
+# Remover valores ausentes e calcular o desvio padrão da variavel.
+sd_ <- sd(na.omit(bladder_msk_2023_clinical_data$"Tumor.Purity"))
+# Visualizar o valor
+sd_
+
+# Remover valores ausentes e calcular o Intervalo interquartilico.
+IQR_ <- IQR(na.omit(bladder_msk_2023_clinical_data$"Tumor.Purity"))
+# Visualizar o valor
+IQR_
+
+barplot(table(bladder_msk_2023_clinical_data$"Tumor.Purity"))
+plot(table(bladder_msk_2023_clinical_data$"Tumor.Purity"))
+
+# Criar o gráfico de caixa
+boxplot(bladder_msk_2023_clinical_data$"Tumor.Purity", horizontal=T)
 
